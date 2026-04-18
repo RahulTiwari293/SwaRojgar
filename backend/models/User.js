961a3c0c6578c91b68e805da4f1ff3fbacee52f0
rahultiwari293 <rahulrocks2932005@gmail.com>
2026-04-19 03:16:40 +0530

    phone number not required for clerk users

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
     firstName:    { type: String, required: true },
     lastName:     { type: String, required: true },
     email:        { type: String, required: true, unique: true, lowercase: true, trim: true },
-    phoneNumber:  { type: String, required: true },
+    phoneNumber:  { type: String, default: '' },
     password:     { type: String, required: true },
     userType: {
         type: String,
